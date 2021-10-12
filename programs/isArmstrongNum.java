000eb8885dd7627cb971e7fa5402c9e18731ed0c
kishanmak2021 <91701359+kishanmak2021@users.noreply.github.com>
2021-10-12 11:48:08 -0700

    Update isArmstrongNum.java

--- programs/isArmstrongNum.java
+++ programs/isArmstrongNum.java
@@ -1,6 +1,6 @@
 mport java.util.Scanner;
 import java.lang.Math;
-public class ArmstsrongNumberExample2 {
+public class isArmstsrongNum {
   //function to check if the number is Armstrong or not  
   static boolean isArmstrong(int n) {
     int temp, digits = 0, last = 0, sum = 0;
